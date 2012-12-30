2d7955962ba975b936f6bf327b4261530a310e44
leonardo <leonardo@leonardo-N53SV.(none)>
2012-12-30 01:45:29 -0300

    documentacion

--- distribucion_empirica.m
+++ distribucion_empirica.m
@@ -1,12 +1,22 @@
-% distribucion de probabilidad empírica de una muestra dada
-n = 1000;
-p = sort(normrnd(0,1,[1 n]));
-q = (1:length(p))/length(p);
+%% distribucion de probabilidad empírica de una muestra dada
+% Algoritmo para obtener la distribución empírica de los datos
+n = 100;
+g = 10;
+p = sort(normrnd(0,1,[1 n]))';
+q = (1:length(p))/length(p)';
 
-% se puede observar que se puede representar la distribución acumulada
-plot(p,q);
+%% con esta información generamos un siste de ecuaciones para obtener el
+% polinomio por minimos cuadrados, con esto el valor de 
+%%
+% 
+% $$A \in \bf{R}$$
+% 
 
-% al encontrar la derivada de la distribucion acumulada
-x = diff(p);
-y = diff(q);
+A = [];
+for i = [0:g]
+    A = [A p.^i];
+end
+
+coef = inv(A'*A)*A'*q;
+
 plot(p(1:(end-1)),y);
