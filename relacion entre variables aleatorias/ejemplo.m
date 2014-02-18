ff18aba3639fa0a57c3222dd2e2b819d5ea245e7
leonardo <ljofre2146@gmail.com>
2014-02-18 08:44:48 -0300

    algunos cambios al documento de ordenamiento de funciones

--- relacion entre variables aleatorias/ejemplo.m
+++ relacion entre variables aleatorias/ejemplo.m
@@ -1,22 +1,58 @@
 % ejemplo de como descubrir la relacion no lineal entre variables
 % aleatorias
-k = 3; % grado del polinomio
+
 n = 100;
-X = normrnd(4,1,1,n);
-Y = X.^k + normrnd(0,20,1,n); % funcion transformada con ruido considerable
+X = linspace(-5,5,n);
+Y = X.^3 - X.^2 + 1 + normrnd(0,25,1,n); % funcion transformada con ruido considerable
 
-% encontrar las funciones asociadas
-Fx = [sort(X),(0:(length(X)-1))/(length(X)-1)];
-Fy = [sort(Y),(0:(length(Y)-1))/(length(Y)-1)];
+for j = 1:3
+    
+    if j~=1
+        Y2 = interp1(f,sort(X),Y2);
+    else
+        Y2 = Y;
+    end
+    
+    % encontrar las funciones asociadas
+    Fx = [sort(X), (0:(length(X)-1))/(length(X)-1)];
+    Fy = [sort(Y2), (0:(length(Y2)-1))/(length(Y2)-1)];
+    
+    % econtrar la relacion entre las variables dada una función f
+    
+    % cuidado: no se pueden repetir los valores en sort(X), o sea, se
+    % requiere filtrar por el promedio
+    u = sort(unique(X));
+    u2 = zeros(size(u));
+    
+    k = (0:(length(X)-1))/(length(X)-1);
+    
+    for i = 1:length(u)
+        u2(i) = mean(k(sort(X)==u(i)));
+    end
+    a = interp1(u, u2, X);
+    
+    v = sort(unique(Y2));
+    v2 = zeros(size(v));
+    
+    k = (0:(length(Y2)-1))/(length(Y2)-1);
+    for i = 1:length(v)
+        v2(i) = mean(k(sort(Y2)==v(i)));
+    end
+    
+    f = interp1(v2,v,a);
+    
+    % acumular la información de la función que relaciona las variables
+    if j~=1
+        f_acum=interp1(Y2,f,f_acum);
+    else
+        f_acum = f;
+    end   
+    plot(X,Y2)
+end
 
-% econtrar la relacion entre las variables dada una función f
-t = linspace(min(X),max(X),n);
-a = interp1(sort(X),(0:(length(X)-1))/(length(X)-1),t);
-f = interp1((0:(length(Y)-1))/(length(Y)-1),sort(Y),a);
 
-plot(t,t.^k)
-hold on;
-plot(X,Y,'o')
-plot(t,f)
-hold off;
+
+
+% si la relacion entre las variables no es biyectiva se puede separar en
+% conjuntos biyectivos.
 
